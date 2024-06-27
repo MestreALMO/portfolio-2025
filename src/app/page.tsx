32435e860eafafecfe0235dd3119ab24560c188d
Andre Lusegardis <lusegardis@gmail.com>
2024-06-27 20:14:59 -0300

    home page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { ThemeSwitcher } from "@/components/themeSwitcher";
+import { useTranslations } from "next-intl";
+import homeImage from "@/imgs/home.jpg";
 
 export default function Home() {
+  const t = useTranslations("pages.home");
+
   return (
-    <main className={styles.main}>
-      <h1>Hello World</h1>
+    <main className={`${styles.homeMain}`}>
+      <div className={`${styles.homeImg}`}>
+        <Image
+          alt="André Lusegardis"
+          src={homeImage}
+          layout="responsive"
+          objectFit="contain"
+          style={{ borderRadius: "50%" }}
+          loading="lazy"
+        />
+      </div>
+      <h1>{t("title")}</h1>
+      <p className={`${styles.homeText}`}>{t("text1")}</p>
+      <p className={`${styles.homeText}`}>{t("text2")}</p>
     </main>
   );
 }
